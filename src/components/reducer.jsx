339e5e39e83b7950cfb93e39ee00b4fa37fdaf2b
Kosi <iyiegbuk@gmail.com>
2022-06-29 16:29:55 +0100

    added remove item function

--- src/components/reducer.jsx
+++ src/components/reducer.jsx
@@ -1,8 +1,17 @@
+// import CartItem from "./cartItem";
+
 const reducer = (state, action) => {
-  if (action.type === "CLEAR_CART") {
-    return { ...state, cart: [] };
+  switch (true) {
+    case action.type === "CLEAR_CART":
+      return { ...state, cart: [] };
+    case action.type === "REMOVE":
+      return {
+        ...state,
+        cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
+      };
+    default:
+      return state;
   }
-  return state;
 };
 
 export default reducer;
